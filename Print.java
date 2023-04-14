defa119cbd1eeabe20368313a362a1bd9433b442
Mercy <sebastinamercy940@gmail.com>
2023-04-14 12:04:01 +0530

    First changes made

---
+++ Print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Print
+
+{
+
+    public static void main(String[] arg) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
